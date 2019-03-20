8d6749973942473566b591b866b9a30dcc1bd287
hannah <story645@gmail.com>
2019-03-20 12:13:28 -0400

    palindromes

--- code/chap08/palindrome.cpp
+++ code/chap08/palindrome.cpp
@@ -3,9 +3,28 @@
 #include <stack>
 #include <iostream>
 #include <iomanip>
+#include <cassert>
 
-bool is_palindrome(const std::string& expression){
-    //postcondition: A true return value indicates that word reads the same forwards and when reversed
+bool is_palindrome(std::string expression){
+    /**postcondition: A true return value indicates that word reads 
+    the same forwards and when reversed**/
+    std::stack <char> reverse;
+    std::queue <char> forward;
+    char letter;
+    for(uint i=0; i<expression.length(); i++){
+        letter = std::tolower(expression[i]);
+        reverse.push(letter);
+        forward.push(letter);
+    }
+    assert(reverse.size()==forward.size());
+    while(!forward.empty()){
+        if(reverse.top()!=forward.front()){
+            return false;
+        }
+        reverse.pop();
+        forward.pop();
+    }  
+    return true;
 }
 
 int main(){
